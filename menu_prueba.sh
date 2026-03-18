08e2d16b31f74744d9f51b4105ee7266589ee768
mbelenmiramontes <mmiramontes@fi.uba.ar>
2026-03-18 15:32:45 -0300

    actualizo la prueba

--- menu_prueba.sh
+++ menu_prueba.sh
@@ -1,3 +1,11 @@
+comprobar_entorno() {
+	[[ -d "EPNro1/entrada" && -d "EPNro1/salida" && -d "EPNro1/proceso" ]]
+}
+
+comprobar_filename() {
+	[[ -n "$FILENAME" && -f "EPNro1/salida/$FILENAME.txt" ]]
+}
+
   echo "⏔⏔⏔⏔⏔⏔ ꒰ MENÚ ꒱⏔⏔⏔⏔⏔⏔ "
   echo "╰┈➤. Opción 1) Crear entorno"
   echo "╰┈➤. Opción 2)  Correr proceso"
@@ -14,18 +22,57 @@ do
   read -p "Porfavor, elegí una opcion: " opcion
   echo ""
 
+
+	if [[ $opcion != "1" && $opcion != "6"]]; then
+	  if ! comprobar_entorno; then
+	      echo "Error: el entorno esta incompleto, por favor seleccionar la opcion 1 primero."
+	      continue
+	  fi
+	fi
+
   case $opcion in
 	1)
-	  if [ -d "EPNro1" ]; then
-	  	echo "El entorno EPNro1 ya existe."
+	  if comprobar_entorno; then
+	  	echo "El entorno EPNro1 y sus subcarpetas ya existen."
 	  else
            	mkdir -p EPNro1/entrada EPNro1/procesado EPNro1/salida
             	echo "Creación de entorno EPNro1 exitoso."
 	  fi
-          ;;
+        ;;
 	2)
-	  if [! -f "EPNro1/consolidar.sh ]; then
+	  if [ ! -f "EPNro1/consolidar.sh ]; then
+		cat << 'EOF' > EPNro1/consolidar.sh
+		#!/bin/bash
 	
+		EOF
+		bash consolidar.sh
+	  else
+		bash consolidar.sh
+	  fi
+	3)
+	  if comprobar_filename(); then
+	    sort -n -k1 "EPNro1/salida/$FILENAME.txt"
+	  else
+	    echo "No existe el archivo $FILENAME. Por favor hacer el paso 2 primero."
+	  fi
+	;;
+	4)
+	  if comprobar_filename(); then
+	    sort -k5 -n -z | head -10 "EPNro1/salida/$FILENAME.txt"
+	  else
+	    echo "No existe el archivo $FILENAME. Por favor hacer el paso 2 primero."
+	  fi
+	;;
+	5)
+	  if comprobar_filename(); then
+	    while true
+	    do
+	    	echo "escribir un numero de padron: "
+	    	read padron
+		  
+		  if padron 
+
+	;;
         6)
           echo -e "Saliendo..."
           exit
@@ -35,4 +82,3 @@ do
           ;;
     esac
 done
-
